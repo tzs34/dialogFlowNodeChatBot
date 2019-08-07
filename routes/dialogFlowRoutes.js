f420888b042191a835ba6582e2b27a73586a84d9
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-08-07 14:02:01 +0100

    Tidied up routing code

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -9,16 +9,7 @@ module.exports = (app) =>{
     app.get('/api/df_text_query', async (req, res) => {
         
           let response = await chatBot.textQuery(req.body.text, req.body.parameters)
-          const result = response[0].queryResult;
-          console.log(`  Query: ${result.queryText}`);
-          console.log(`  Response: ${result.fulfillmentText}`);
-          if (result.intent) {
-            console.log(`  Intent: ${result.intent.displayName}`);
-          } else {
-            console.log(`  No intent matched.`);
-          }
-
-        res.send(response)
+          res.send(response[0].queryResult);
     })
     
     app.get('/api/df_event_query', (req, res) => {
