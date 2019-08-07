bc6784e56b75de624f11ade3915706059d37e0ca
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-08-07 13:27:58 +0100

    Added parameters pass to queryText function of chatbot

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app) =>{
     
     app.get('/api/df_text_query', async (req, res) => {
         
-          let response = await chatBot.textQuery(req.body.text)
+          let response = await chatBot.textQuery(req.body.text, req.body.parameters)
           const result = response[0].queryResult;
           console.log(`  Query: ${result.queryText}`);
           console.log(`  Response: ${result.fulfillmentText}`);
