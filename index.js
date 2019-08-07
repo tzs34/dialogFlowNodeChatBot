2b1fb4f76249d2ebad64ff8421d70d89c8e3bf81
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-08-07 13:47:29 +0100

    Removed conflict tag

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express  = require('express')
 const bodyParser = require('body-parser')
 const dialogFlowRoutes = require('./routes/dialogFlowRoutes')
@@ -10,17 +9,4 @@ dialogFlowRoutes(app)
 
 const PORT = process.env.PORT || 5000
 
-=======
-const express  = require('express')
-const bodyParser = require('body-parser')
-const dialogFlowRoutes = require('./routes/dialogFlowRoutes')
-const app = express()
-
-app.use(bodyParser.json())
-dialogFlowRoutes(app)
-
-
-const PORT = process.env.PORT || 5000
-
->>>>>>> 4b2bf95dbc8f5e1e60a1730ef9b60bb6a5b0ffa4
 app.listen(PORT)
